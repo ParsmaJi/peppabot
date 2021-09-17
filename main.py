6ba87681113592dae5557bf0450843208973fe80
ParsmaJi <83138062+ParsmaJi@users.noreply.github.com>
2021-09-18 00:33:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-client.run('ODg2Njc4NTU0ODI4NTA1MTY4.YT5FwQ.XmTdflHgIdaaAYWhNPAdMke86bg')
+client.run('token')
